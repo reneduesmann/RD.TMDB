3c6c891342e2e5f92f177e3eaf2719f73fae010a
René Duesmann <duesmannr96@gmail.com>
2024-04-25 10:57:59 +0200

    feat: Added setup for unit tests

---
+++ RD.TMDB.Core.UnitTests/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using RD.TMDB.Core.Contracts.Factories;
+using RD.TMDB.Core.Providers;
+
+namespace RD.TMDB.Core.UnitTests;
+
+public abstract class TestBase
+{
+    protected TmdbClient Client { get; }
+
+    protected string BearerToken { get; }
+
+    public IRefitApiFactory RefitApiFactory { get; set; }
+
+    protected TestBase()
+    {
+        this.BearerToken = "emptyToken";
+        this.RefitApiFactory = A.Fake<IRefitApiFactory>();
+        this.Client = new TmdbClient(this.BearerToken, this.RefitApiFactory);
+    }
+}
